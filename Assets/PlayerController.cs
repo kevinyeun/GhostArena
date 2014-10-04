855af2a059896cacd521461a5b762b368f26f700
kevinyeun <kevinyeun@berkeley.edu>
2014-10-04 02:48:46 -0700

    Test scene 1

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour
+{
+
+	public Transform background;
+
+	// Use this for initialization
+	void Start ()
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		Vector3 oldPos = transform.position;
+
+		if (Input.GetKeyDown ("right")) {
+			transform.position = new Vector3 (oldPos.x + 0.5f, oldPos.y, oldPos.z);
+		} else if (Input.GetKeyDown ("left")) {
+			transform.position = new Vector3 (oldPos.x - 0.5f, oldPos.y, oldPos.z);
+		}
+	}
+}
