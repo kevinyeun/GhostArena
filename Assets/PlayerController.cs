a4e2fdf71b64275d17921c698edd81fd770043ab
kevinyeun <kevinyeun@berkeley.edu>
2014-10-04 04:09:40 -0700

    a

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -12,7 +12,7 @@ public class PlayerController : MonoBehaviour
 	void Start ()
 	{
 		transform.localScale = new Vector3 (1f / NUM_TILES, 1f / NUM_TILES, 1);
-		speed = 5f / NUM_TILES;
+		speed = 0.03f*5f / NUM_TILES;
 	}
 	
 	// Update is called once per frame
@@ -21,19 +21,19 @@ public class PlayerController : MonoBehaviour
 		Vector3 oldPos = transform.position;
 		Vector3 oldScale = transform.localScale;
 
-		if (Input.GetKeyDown ("right")) {
+		if (Input.GetKey ("right")) {
 			transform.position = new Vector3 (oldPos.x + speed, oldPos.y, oldPos.z);
 			transform.localEulerAngles = new Vector3 (0, 0, 90f);
 			mainCamera.localEulerAngles = new Vector3 (0, 0, -90f);
-		} else if (Input.GetKeyDown ("left")) {
+		} else if (Input.GetKey ("left")) {
 			transform.position = new Vector3 (oldPos.x - speed, oldPos.y, oldPos.z);
 			transform.localEulerAngles = new Vector3 (0, 0, 270f);
 			mainCamera.localEulerAngles = new Vector3 (0, 0, -270f);
-		} else if (Input.GetKeyDown ("up")) {
+		} else if (Input.GetKey ("up")) {
 			transform.position = new Vector3 (oldPos.x, oldPos.y + speed, oldPos.z);
 			transform.localEulerAngles = new Vector3 (0, 0, 180f);
 			mainCamera.localEulerAngles = new Vector3 (0, 0, -180f);
-		} else if (Input.GetKeyDown ("down")) {
+		} else if (Input.GetKey ("down")) {
 			transform.position = new Vector3 (oldPos.x, oldPos.y - speed, oldPos.z);
 			transform.localEulerAngles = new Vector3 (0, 0, 0f);
 			mainCamera.localEulerAngles = new Vector3 (0, 0, -0f);
